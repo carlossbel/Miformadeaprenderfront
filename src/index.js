6a8463c91f19163e0813c2fcbe27c2bd7eca0584
AnetteeYunuen <anetteyunuenruizm@gmail.com>
2024-10-21 13:41:28 -0600

    prueba index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppRouter from './router'; // Cambia App por AppRouter para manejar las rutas
+import AppRouter from './router'; // Cambia App por AppRouter para manejar las rutas jjjjjjjjj
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
