9b8f41cfc67babc225a756b7bfd05a6f535ffbbb
Carlos Beltran <carlossbel09@gmail.com>
2025-04-08 16:01:51 -0600

    first commit

--- src/alertService.js
+++ src/alertService.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CustomAlert from './CustomAlert';
+import CustomAlert from './components/CustomAlert/CustomAlert';
 
 // Crear un contenedor para las alertas si no existe
 const getAlertContainer = () => {
