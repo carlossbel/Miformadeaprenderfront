cd4a301b93a242f8724f7b6389f2df32b1a50fd7
Carlos Beltran <carlossbel09@gmail.com>
2025-04-08 12:00:34 -0600

    first commit

--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ import Kinestésico from './kinestesico';
 
 function AppRouter() {
   return (
-    <Router basename="/miformadeaprender">
+    <Router>
       <Routes>
         <Route path="/" element={<App />} /> 
         <Route path="/login" element={<Login/>} /> 
